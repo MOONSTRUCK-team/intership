13e06c117a4fa1dd2ed944553cddc006e76cb58c
Merisa <besiroviccm@gmail.com>
2023-12-05 23:08:43 +0100

    low-level-calls + difference between transfer and send

---
+++ presentations/low-level-calls/delegatecall.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+
+contract B {
+    uint public num;
+    address public sender;
+    uint public value;
+
+    function setVars(uint _num) public payable {
+       num = _num +1;
+        sender = msg.sender;
+        value = msg.value;
+    }
+}
+contract B2 {
+
+    uint public num;
+    address public sender;
+    uint public value;
+
+    function setVars(uint _num) public payable {
+        num = _num * 2;
+        sender = msg.sender;
+        value = msg.value;
+    }
+}
+
+contract A {
+    uint public num;
+    address public sender;
+    uint public value;
+
+    function setVars(address _contract, uint _num) public payable {
+        (bool success, bytes memory data) = _contract.delegatecall(
+            abi.encodeWithSignature("setVars(uint256)", _num)
+        );
+    }
+}
